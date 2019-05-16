9c0e0599c815d7ec1658b0c82e48e941c0ac5e92
focused220 <roblhjr.220@gmail.com>
2019-05-16 13:04:07 -0700

    set up for deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
-
+require('dotenv').config();
 const server = require('./server.js');
+const port = process.env.PORT || 9090;
 
-server.listen(9090, () =>{
-    console.log('\n Server Running on port 9090')
+server.listen(port, () =>{
+    console.log(`\n Server Running on port ${port}`)
 })
