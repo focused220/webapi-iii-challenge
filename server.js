706657b2334b9470d2317d6e62a05403301fd037
focused220 <roblhjr.220@gmail.com>
2019-05-15 17:39:12 -0700

    User Db routes created

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+
+const UserRouter = require('./users/userRouter')
 
 const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+
+server.use(reqLogger);
+
+server.use('/users', UserRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {
-
+function reqLogger(req, res, next) {
+  const now = new Date();
+  const method = req.method;
+  const url = req.url;
+    console.log(`\n${now}\n${method}\n${url}`)
+    next();
 };
 
 module.exports = server;
